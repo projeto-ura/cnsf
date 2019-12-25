1283b8fa7425f7ec445827f254a4690250641ad7
projeto-ura <uragerente@gmail.com>
2019-12-24 21:49:24 -0300

    Updates

---
+++ precache-manifest.94e4ee663f1201f2fbecd8c4faf0a207.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3b36589801597bff1b5d6ca674b00a6",
+    "url": "/cnsf/index.html"
+  },
+  {
+    "revision": "5c284b82b8943cf067eb",
+    "url": "/cnsf/static/css/main.ae021341.chunk.css"
+  },
+  {
+    "revision": "2ce255df34c71b243958",
+    "url": "/cnsf/static/js/2.aa1c246f.chunk.js"
+  },
+  {
+    "revision": "5c284b82b8943cf067eb",
+    "url": "/cnsf/static/js/main.557d1ffa.chunk.js"
+  },
+  {
+    "revision": "c9e6e182aff784fdbca6",
+    "url": "/cnsf/static/js/runtime-main.ee338c28.js"
+  },
+  {
+    "revision": "ae9e21f5cf9ea2c2643eb8c6dfd0bac6",
+    "url": "/cnsf/static/media/1.ae9e21f5.jpg"
+  },
+  {
+    "revision": "23f4f6c116d3fdf9980acd98b184602a",
+    "url": "/cnsf/static/media/2.23f4f6c1.jpg"
+  },
+  {
+    "revision": "9e90b401da24abce7f60d292f0d67e93",
+    "url": "/cnsf/static/media/3.9e90b401.jpg"
+  },
+  {
+    "revision": "a7eb41923d120c9973742501fadde997",
+    "url": "/cnsf/static/media/background.a7eb4192.png"
+  },
+  {
+    "revision": "00ca36933f4572f681bc61e786bb14c6",
+    "url": "/cnsf/static/media/banner.00ca3693.png"
+  },
+  {
+    "revision": "8f5ce27564945d2c9a10ef827549a78c",
+    "url": "/cnsf/static/media/facebook.8f5ce275.png"
+  },
+  {
+    "revision": "7e34628d7598ea9cb8ea3c2015792f74",
+    "url": "/cnsf/static/media/logotipo.7e34628d.png"
+  },
+  {
+    "revision": "ee42bf0bf151e66e3cc9f088f7709398",
+    "url": "/cnsf/static/media/metologia.ee42bf0b.jpg"
+  },
+  {
+    "revision": "d83f15b80c968a5a0f408d1e12681bf9",
+    "url": "/cnsf/static/media/school.d83f15b8.svg"
+  },
+  {
+    "revision": "855adeac81c7ea5413aa0f894c76a301",
+    "url": "/cnsf/static/media/study.855adeac.svg"
+  },
+  {
+    "revision": "fd0dee780c3c245bc722f6b1499e3dcd",
+    "url": "/cnsf/static/media/teacher.fd0dee78.svg"
+  },
+  {
+    "revision": "e44c8f30f0ba031fbcc48e09bb9baf24",
+    "url": "/cnsf/static/media/training.e44c8f30.svg"
+  },
+  {
+    "revision": "0251d8ee95aa6d1f3400faa3b46b4bcf",
+    "url": "/cnsf/static/media/twitter.0251d8ee.png"
+  }
+]);
