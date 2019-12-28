368cd201f4b6dbdba6162d7100e2f16b007e378f
projeto-ura <uragerente@gmail.com>
2019-12-27 23:18:20 -0300

    routes

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react' 
 import { AwesomeButton, AwesomeButtonProgress, AwesomeButtonSocial } from 'react-awesome-button';
+import { HashRouter, Route, Link } from "react-router-dom";
+
 import Styless from './react-awesome-button-master/src/styles/themes/theme-blue/index';
 
 
@@ -18,12 +20,13 @@ import logo from '../../img/logotipo.png'
 export default class Header extends Component {
     render() {
         return (
+            <HashRouter basename="/">
                 <header className="main-header">
 
                     <div className="header-content">
 
                         <div className="header-logo">
-                            <a href="/" className="a-logo"><img src={logo} className="img-logo" alt=""></img></a>
+                            <Link to="/" className="a-logo"><img src={logo} className="img-logo" alt=""></img></Link>
                         </div>
 
 
@@ -33,7 +36,7 @@ export default class Header extends Component {
                                 <li className="list"><a  className="btn"><AwesomeButton type="primary" size="medium" >Colégio</AwesomeButton></a>
 
                                         <ul className="drop">
-                                            <li><a href="#"><AwesomeButton type="primary" size="small">Metologia</AwesomeButton></a></li>
+                                            <li><Link to="/Metologia"><AwesomeButton type="primary" size="small">Metologia</AwesomeButton></Link></li>
                                             <li><a href="#"><AwesomeButton type="primary" size="small">Estrutura</AwesomeButton></a></li>
                                             <li><a href="#"><AwesomeButton type="primary" size="small">Fundação</AwesomeButton></a></li>
                                         </ul>
@@ -76,7 +79,8 @@ export default class Header extends Component {
                     </div>
 
                     
-                </header>    
+                </header>
+            </HashRouter>      
         )
     }
 }
